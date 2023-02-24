fa99fb92a23e4888c39702935c01bba3fa49ccd2
Levan Bokeria <bokerial3o@gmail.com>
2023-02-24 11:23:16 +0400

    Hide-show

---
+++ main.js
@@ -0,0 +1,8 @@
+let search = document.querySelector(".search")
+let icon = document.querySelector(".icon")
+let expanded = "200px"
+let srchicon = document.querySelector(".srchicon")
+icon.addEventListener("click", function(){
+    search.classList.toggle("tog")
+})
+
